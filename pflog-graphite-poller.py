7205d2671939f601e8518c4402d86cdeeeb5c2fb
Jeffrey Forman <jeff@jeffreyforman.net>
2013-01-19 17:54:11 -0500

    we don't care about the source port, so lets simplify for the time being

--- pflog-graphite-poller.py
+++ pflog-graphite-poller.py
@@ -24,14 +24,14 @@ import statsd_client
 
 
 RE_HOSTNAME = re.compile(r"(?P<short_name>\w+).?")
-RE_TCPDUMP_IPV4 = re.compile(r"""(?P<datetime_stamp>.*?) rule (?P<rule_number>\d+)/\(match\) (?P<action>(block|pass)) (?P<direction>(in|out)) on (?P<interface>\w+): (?P<src_ip>([0-9]{1,3}\.){3}[0-9]{1,3})\.?(?P<src_port>\d{1,5})? > (?P<dest_ip>([0-9]{1,3}\.){3}[0-9]{1,3})\.?(?P<dest_port>\d{1,5})?""")
+RE_TCPDUMP = re.compile(r"""(?P<datetime_stamp>.*?) rule (?P<rule_number>\d+)/\(match\) (?P<action>(block|pass)) (?P<direction>(in|out)) on (?P<interface>\w+): (?P<src_ip>\S+) > (?P<dest_ip>\S+)\s""")
 
 HOSTNAME = RE_HOSTNAME.search(socket.gethostname()).group("short_name")
 
 
 def send_to_statsd(args, tcpdump_dict):
     """Given a stat path, increment statistic in statsd."""
-    stat_path = "%(hostname)s.pf.%(interface)s.rule%(rule_number)s.%(direction)s.%(action)s" % tcpdump_dict
+    stat_path = "pf.%(hostname)s.%(interface)s.rule%(rule_number)s.%(direction)s.%(action)s" % tcpdump_dict
     logging.info("Incrementing statsd path: %s", stat_path)
     statsd_client.Statsd.increment(args, stat_path)
     logging.debug("Done incrementing statsd.")
@@ -39,7 +39,7 @@ def send_to_statsd(args, tcpdump_dict):
 
 def get_args():
     """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description="Process pflog for monitoring, alerting, etc.")
+    parser = argparse.ArgumentParser(description="Send pflog output from tcpdump to statsd for Graphite display.")
     parser.add_argument("--debug",
                         action="store_true",
                         help="Enable debug output.")
@@ -56,7 +56,7 @@ def get_args():
 def parse_tcpdump_line(raw_tcpdump_line):
     """Given tcpdump output line, parse into a Graphite-friendly statistic."""
     current_line = raw_tcpdump_line.strip()
-    parsed_line = RE_TCPDUMP_IPV4.search(current_line)
+    parsed_line = RE_TCPDUMP.search(current_line)
     if parsed_line is None:
         return None
 
@@ -66,9 +66,7 @@ def parse_tcpdump_line(raw_tcpdump_line):
                      "direction" : parsed_line.group("direction"),
                      "interface" : parsed_line.group("interface"),
                      "src_ip" : parsed_line.group("src_ip"),
-                     "src_port" : parsed_line.group("src_port"),
                      "dest_ip" : parsed_line.group("dest_ip"),
-                     "dest_port" : parsed_line.group("dest_port"),
                      }
     return tcpdump_dict
 
@@ -111,7 +109,6 @@ def main():
             logging.debug("Tcpdump dict: %s", tcpdump_dict)
             if tcpdump_dict:
                 tcpdump_dict["hostname"] = HOSTNAME
-                logging.info("Matching pflog entry found.")
                 if args.statsd:
                     send_to_statsd(args, tcpdump_dict)
     except KeyboardInterrupt:
